288051e6ca56a4eb8f9f04e7c9a824c0d2793f42
Lewis-Le <73074166+Lewis-Le@users.noreply.github.com>
2022-01-08 23:06:44 +0700

    food panel giỏ hàng current, add/remove in giỏ hàng current

--- pages/foods/index.js
+++ pages/foods/index.js
@@ -7,11 +7,40 @@ import { useState, useEffect, useRef } from 'react';
 import Layout from '../../components/Layout'
 
 
+function Foods_panel({ list, list_id }) {
+
+  // console.log(list_id);
+
+  return (
+    <>
+      <div className="w-full h-full flex items-end justify-center pointer-events-none fixed">
+        <div className="bg-carrot flex justify-center gap-3 p-4 rounded-lg w-4/6 h-2/6">
+          {
+            list_id.map(id => {
+              console.log(list[id]);
+              return <>
+                <div className="w-24 animate__animated animate__zoomInDown">
+                  <img src={list[id].img} />
+                  <p>{list[id].title}</p>
+                </div>
+              </>
+            })
+          }
+        </div>
+      </div>
+
+    </>
+  );
+};
+
+
+
 export default function App() {
 
   const [foods_data, setFoods_data] = useState([]);   //data get từ server
   const [current_list_foods_order, setCurrent_list_foods_order] = useState([]);  //là array chứa các obj foods 
   const [current_total_price, setCurrent_total_price] = useState(0);
+  const [list_id, setList_id] = useState([]);
 
 
   useEffect(() => {
@@ -26,21 +55,27 @@ export default function App() {
     // console.log(data)
   };
 
+
   function add_more_food(e) {
     let btn_div_parent = e.target.parentNode;
     let div_parent = btn_div_parent.parentNode;
     btn_div_parent.childNodes[0].classList.remove('hidden');  //hiện nút remove
     // console.log(div_parent);
+    if (list_id.indexOf(div_parent.id) === -1) {    //nếu chưa có id món này trong list thì push thêm vào
+      list_id.push(div_parent.id);
+      setList_id(list_id);
+    };
     current_list_foods_order[`${div_parent.id}`] = {    //add/update thông tin món này vào đơn hàng
       id_food: div_parent.id,
       title: div_parent.childNodes[1].textContent,
+      img: div_parent.childNodes[0].src,
       note: '',
       total: `${parseInt(btn_div_parent.childNodes[1].textContent) + 1}`,
       price: div_parent.childNodes[3].textContent,
       total_price: div_parent.childNodes[3].textContent * (parseInt(btn_div_parent.childNodes[1].textContent) + 1),
     };
     setCurrent_list_foods_order(current_list_foods_order);
-    console.log(current_list_foods_order);
+    // console.log(current_list_foods_order);
     btn_div_parent.childNodes[1].textContent = `${parseInt(btn_div_parent.childNodes[1].textContent) + 1}`;   //tăng sl món ăn
     setCurrent_total_price(current_total_price + parseInt(current_list_foods_order[`${div_parent.id}`].price));   //Tính tổng số tiền đơn hàng
     //giảm phần món ăn xuống trong allfoods
@@ -53,12 +88,14 @@ export default function App() {
     if (btn_div_parent.childNodes[1].textContent === '1') {   //ẩn nút remove
       // current_list_foods_order.splice(`${div_parent.id}`, 1);
       // console.log(current_list_foods_order.indexOf(current_list_foods_order[`${div_parent.id}`]))
+      setList_id(list_id.filter(id => id != div_parent.id));  //xóa id món này khỏi list
       e.target.classList.add('hidden');
     }
     else if (btn_div_parent.childNodes[1].textContent != '1') {
       current_list_foods_order[`${div_parent.id}`] = {    //add/update thông tin món này trong đơn hàng
         id_food: div_parent.id,
         title: div_parent.childNodes[1].textContent,
+        img: div_parent.childNodes[0].src,
         note: '',
         total: `${parseInt(btn_div_parent.childNodes[1].textContent) - 1}`,
         price: div_parent.childNodes[3].textContent,
@@ -67,7 +104,7 @@ export default function App() {
     };
     // console.log(div_parent);
     setCurrent_list_foods_order(current_list_foods_order);
-    console.log(current_list_foods_order);
+    // console.log(current_list_foods_order);
     btn_div_parent.childNodes[1].textContent = `${parseInt(btn_div_parent.childNodes[1].textContent) - 1}`;   //giảm sl món ăn
     setCurrent_total_price(current_total_price - parseInt(current_list_foods_order[`${div_parent.id}`].price));   //Tính tổng số tiền đơn hàng
     //tăng phần món ăn lên trong allfoods
@@ -84,6 +121,7 @@ export default function App() {
     <Layout>
       <p className="text-center text-lg">Tất cả món ăn</p>
       <p>Tổng giá trị thanh toán: {current_total_price}.000 VND</p>
+      <Foods_panel list={current_list_foods_order} list_id={list_id} />
       <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5 px-12 mt-7">
         {
           foods_data?.data?.map(food => {
